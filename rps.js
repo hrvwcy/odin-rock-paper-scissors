bb94d9e73abaa68acf36863e16941872668ead0f
Harvey <harveywcyeo@gmail.com>
2025-05-26 10:28:28 +0700

    Defined computer move and human move

---
+++ rps.js
@@ -0,0 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+
+// function getComputerChoice() {
+//     const computerMove = Math.floor(Math.random() * 3);
+
+//     if (computerMove === 0) {
+//         console.log("Rock")
+//     } else if (choiceNumber === 1) {
+//         console.log("Paper")
+//     } else
+//         console.log ("Scissors")
+// }
+
+// function getHumanChoice() {
+//     const humanMove = prompt("Input your choice:");
+//     console.log(humanMove);
+// }
+
+// getComputerChoice()
+
+// getHumanChoice()
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
